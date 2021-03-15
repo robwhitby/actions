0cec829bfe0eab9c47c96ea0969a8757b7fe518f
Rob Whitby <rob.whitby@springer.com>
2021-03-15 11:01:55 +0000

    init

---
+++ runner/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -eu
+cd /home/runner
+
+TOKEN=$(curl -v -X POST -H "authorization: token ${GITHUB_PAT}" "https://api.github.com/orgs/${GITHUB_ORG}/actions/runners/registration-token" | jq -r .token)
+
+./config.sh \
+  --url "https://github.com/${GITHUB_ORG}" \
+  --token "${TOKEN}" \
+  --unattended \
+  --replace
+
+exec ./bin/runsvc.sh
